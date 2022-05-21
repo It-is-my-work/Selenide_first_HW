6285c53c551ed29ebaef49c7725dec8a5ea65a8c
Богданова Алена Михайловна <albogdanova@sberhealth.ru>
2022-05-22 01:52:16 +0300

    Added the second and third parts

--- src/test/java/selenideTests/SelenideTests.java
+++ src/test/java/selenideTests/SelenideTests.java
@@ -1,2 +1,29 @@
-package selenideTests;public class SelenideTests {
+package selenideTests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideTests {
+
+    @Test
+    void SoftAssertionExampleTest(){
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link button").click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5"));
+    }
+
+    @Test
+    void DragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        sleep(5000);
+    }
 }
